f178952e410dfce5c24167c25efb7e69f741a822
Max Mattes <maxbot1539@gmail.com>
2018-09-26 12:21:05 -0400

    Started React frontend (following a different tutorial that acutally works, so some old stuff was removed)

--- ibis/ibis/settings.py
+++ ibis/ibis/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
-    'api',
-    'web'
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +57,9 @@ ROOT_URLCONF = 'ibis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'build/static'),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
--- ibis/ibis/urls.py
+++ ibis/ibis/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('web/', include('web.urls'))
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
